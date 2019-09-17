4493263152d81d9219dbf9258f5964eee390107c
Rhonda Gilliard <amourg08@gmail.com>
2019-09-17 18:50:12 -0400

    updating changes

---
+++ Todays/conditionals.js
@@ -0,0 +1,39 @@
+// > Greater
+// < Less
+// == Equal
+// != Not Equal
+// >= Greater Than
+// <= Less Than
+
+if(3>5) {
+	console.log("3 is gereater than 5");
+} else {
+	console.log("3 is not great than 5");
+}
+
+
+var x=10;
+var y=30;
+
+if(x == y) {
+	console.log("X is equal to Y");
+} else if(x != y){
+	console.log("Hmmm Loop Hole");
+} else {
+	console.log("X is not equal to Y");
+}
+
+
+
+var Marvel = "Marvel";
+var DC = "DC";
+
+if(Marvel == DC) {
+	console.log("They are equal");
+} else if(Marvel > DC) {
+	console.log("Marvel Is Grater");
+} else if(Marvel < DC) {
+	console.log("DC Is Great");
+} else {
+	console.log("Unknown");
+}
--- Todays/formula.js
+++ Todays/formula.js
@@ -6,9 +6,33 @@
 // Customer Churn Rate: The rate at which your customers are cancelling their subscriptions.]
 
 
-var ARP = 200;
-var GM  = 400;
-var CCR = 50; 
-var LTV = ARP * GM/CCR;
 
-console.log(LTV);
+
+var ltvFunction = function(ARPA, MM, churn) {
+	var LTV = (ARPA*MM)/churn;
+
+	return LTV;
+}
+
+console.log(ltvFunction(56,10,2));
+
+console.log("Steve thinks the LTV should be" + ltvFunction(5,10,2));
+
+console.log("John thinks the LTV should be" + ltvFunction(2,4,2));
+
+
+
+
+
+
+var mypie = 3.14;
+
+
+var pieFunction  = function(pie,radius)  {
+	var Circle =(pie*radius*radius);
+
+	return Circle;
+
+}
+
+console.log(pieFunction(mypie,30));
